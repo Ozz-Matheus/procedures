6f01d2c5302ec77b34615d09179e904ca91e7081
Ozz-Matheus <montesinos.quintana@gmail.com>
2025-06-13 18:32:03 -0600

    Detalle de Estilos.

--- template-parts/content-credential-card.php
+++ template-parts/content-credential-card.php
@@ -16,7 +16,7 @@ $fecha_afiliacion = pho_get_affiliation_date($procedure_id);
 
 ?>
 <div class="wpb_column vc_column_container vc_col-sm-12 margin-top-40">
-    <div class="pho-wallet-card margin-bottom-40" style="max-width: 360px; border-radius: 16px; border: 1px solid #ccc; padding: 20px; box-shadow: 0 4px 8px rgba(0,0,0,0.1); font-family: sans-serif; margin-top: 40px;">
+    <div class="pho-wallet-card margin-bottom-40" style="max-width: 360px; border-radius: 16px; border: 1px solid #ccc; padding: 20px; box-shadow: 0 4px 8px rgba(0,0,0,0.1); font-family: sans-serif; margin-top: 40px;     background-color: #ffffff;">
         <?php if ($logo): ?>
             <div style="text-align:center;">
                 <img src="<?php echo esc_url($logo); ?>" style="height: 60px; margin-bottom: 20px;" alt="Logo">
@@ -27,14 +27,14 @@ $fecha_afiliacion = pho_get_affiliation_date($procedure_id);
             </div>
         <?php endif; ?>
 
-        <div style="text-align: center; margin-bottom: 20px;">
+        <div style="text-align: center; margin-top: 20px; margin-bottom: 20px;">
             <?php if ($foto): ?>
                 <img src="<?php echo esc_url($foto); ?>" style="width: 80px; height: 80px; border-radius: 50%; object-fit: cover;">
             <?php else: ?>
                 <div style="width: 80px; height: 80px; border-radius: 50%; background-color: #ccc; display: inline-block;"></div>
             <?php endif; ?>
-            <h2 style="margin: 10px 0 0;"><?php echo esc_html($nombre); ?></h2>
-            <small><strong>N° miembro:</strong> <?php echo esc_html($member_number); ?></small>
+            <h5 style="margin: 10px 0 0;"><?php echo esc_html($nombre); ?></h5>
+            <small style="display: block; margin: 10px 0;"><strong>N° miembro:</strong> <?php echo esc_html($member_number); ?></small>
         </div>
 
         <div style="text-align:center; margin-bottom: 20px;">
